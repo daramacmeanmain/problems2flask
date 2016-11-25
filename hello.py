710cd4939b4a961499c0a20c1cab4b5fe318b3b4
daramacmeanmain <dara.macmeanmain@outlook.ie>
2016-11-25 11:34:29 +0000

    name variable

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/name/<name>') 
+def fname(name): 
+	return 'Your name is ' + name
+	
 @app.route("/")
-
-
 def hello():
     return app.send_static_file('index.html')
 
